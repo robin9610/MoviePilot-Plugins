02c27f7e885714431854ed2b6e52ca72d3c009f0
robin <847242549@qq.com>
2025-09-07 13:02:40 +0800

    更改版本号

--- plugins.v2/st98sign/__init__.py
+++ plugins.v2/st98sign/__init__.py
@@ -27,9 +27,9 @@ class st98sign(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/jxxghp/MoviePilot-Plugins/main/icons/Ferdi_B.png"
     # 插件版本
-    plugin_version = "1.1.0"
+    plugin_version = "2.0.0"
     # 插件作者
-    plugin_author = "Desire"
+    plugin_author = "Robin"
     # 作者主页
     author_url = "https://github.com"
     # 插件配置项ID前缀
